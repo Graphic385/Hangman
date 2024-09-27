c679d908a8046cb05d7ea1ac157d333f402a82ea
Graphic385 <Domguy385@gmail.com>
2024-09-27 19:49:31 -0400

    Implement rendering code

--- src/Main.java
+++ src/Main.java
@@ -1,75 +1,106 @@
 public class Main {
+	public static void main(String[] args) {
+		printHangman(1);
+		printKeyboard();
+		printCategory("Example Category");
+		printWord("_xampl_");
+	}
 
-    public static void main(String[] args) {
-	printHangman(1);
-	printKeyboard();
-	
-    }
-    
-    public static void printHangman(int i) {
-	String[] hangmanpics = {
-		"""
-		  +---+
-		  |   |
-		      |
-		      |
-		      |
-		      |
-		=========""",
-		"""
-		  +---+
-		  |   |
-		  O   |
-		      |
-		      |
-		      |
-		=========""",
-		"""
-		  +---+
-		  |   |
-		  O   |
-		  |   |
-		      |
-		      |
-		=========""",
-		"""
-		  +---+
-		  |   |
-		  O   |
-		 /|   |
-		      |
-		      |
-		=========""",
-		"""
-		  +---+
-		  |   |
-		  O   |
-		 /|\\  |
-		      |
-		      |
-		=========""",
-		"""
-		  +---+
-		  |   |
-		  O   |
-		 /|\\  |
-		 /    |
-		      |
-		=========""",
-		"""
-		  +---+
-		  |   |
-		  O   |
-		 /|\\  |
-		 / \\  |
-		      |
-		=========""" };
-	System.out.println(hangmanpics[i]);
-    }
-    
-    public static void printKeyboard() {
-	System.out.print("\033[0;0H");
-	System.out.println("A B C D E F G H I J K L M\n"
-			 + "N O P Q R S T U V W X Y Z");
-    }
+	public static void printHangman(int i) {
+		String[] hangmanpics = {
+				"   +---+\n" +
+						"   |   |\n" +
+						"       |\n" +
+						"       |\n" +
+						"       |\n" +
+						"       |\n" +
+						" =========",
+
+				"   +---+\n" +
+						"   |   |\n" +
+						"   O   |\n" +
+						"       |\n" +
+						"       |\n" +
+						"       |\n" +
+						" =========",
+
+				"   +---+\n" +
+						"   |   |\n" +
+						"   O   |\n" +
+						"   |   |\n" +
+						"       |\n" +
+						"       |\n" +
+						" =========",
+
+				"   +---+\n" +
+						"   |   |\n" +
+						"   O   |\n" +
+						"  /|   |\n" +
+						"       |\n" +
+						"       |\n" +
+						" =========",
+
+				"   +---+\n" +
+						"   |   |\n" +
+						"   O   |\n" +
+						"  /|\\  |\n" +
+						"       |\n" +
+						"       |\n" +
+						" =========",
+
+				"   +---+\n" +
+						"   |   |\n" +
+						"   O   |\n" +
+						"  /|\\  |\n" +
+						"  /    |\n" +
+						"       |\n" +
+						" =========",
+
+				"   +---+\n" +
+						"   |   |\n" +
+						"   O   |\n" +
+						"  /|\\  |\n" +
+						"  / \\  |\n" +
+						"       |\n" +
+						" ========="
+		};
+		System.out.println(hangmanpics[i]);
+	}
+
+	public static void printKeyboard() {
+		System.out.print("\033[s"); // save cursor position
+		System.out.print("\033[3A"); // move up 3 lines
+		System.out.print("\033[12G"); // move to column 12
+
+		System.out.print("A B C D E F G H I J K L M\n");
+
+		System.out.print("\033[12G"); // move to column 12
+		System.out.print("N O P Q R S T U V W X Y Z");
+
+		System.out.print("\033[u"); // restore cursor position
+	}
+
+	public static void printCategory(String categoryName) {
+		System.out.print("\033[s"); // save cursor position
+		System.out.print("\033[1m"); // bold
+		System.out.print("\033[7A"); // move up 7 lines
+		System.out.print("\033[12G"); // move to column 12
+		System.out.print("\033[" + (13 - categoryName.length() / 2) + "C"); // move to right so text is centered
+
+		System.out.print(categoryName);
+
+		System.out.print("\033[22m"); // disable bold
+		System.out.print("\033[u"); // restore cursor position
+	}
+
+	public static void printWord(String word) {
+		System.out.print("\033[s"); // save cursor position
+		System.out.print("\033[5A"); // move up 7 lines
+		System.out.print("\033[12G"); // move to column 12
+		System.out.print("\033[" + (13 - word.length() / 2) + "C"); // move to right so text is centered
+
+		System.out.print(word);
+
+		System.out.print("\033[u"); // restore cursor position
+	}
 }
